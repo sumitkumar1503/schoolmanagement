eaa1b23cac7c66adf1180cdba6ee9d820cc03870
sumit <wapka1503@gmail.com>
2020-05-08 14:51:32 +0530

    s

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'schoolmanagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-'NAME': 'sms',
-'USER': 'root',
-'PASSWORD': 'root',
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
